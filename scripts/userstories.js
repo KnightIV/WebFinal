bcddf498c35b3e402435a85707dc08e2a38303d3
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2017-03-13 16:58:47 -0600

    Fixed user stories slides

--- scripts/userstories.js
+++ scripts/userstories.js
@@ -1,26 +1,32 @@
+var counter = 1200000000;
+
 $(document).ready(function () {
     $("#prev_image").click(function () {
-        prev();
+        update(-1);
     });
     $("#next_image").click(function () {
-        next();
+        update(1);
     });
+
+
 });
 // Write all the names of images in slideshow
 var images = ["images/UserStories/Fantasy1.jpg", "images/UserStories/Fantasy2.jpg", "images/UserStories/Fantasy3.jpg", "images/UserStories/PostApocalyptic1.jpg", "images/UserStories/PostApocalyptic2.jpg", "images/UserStories/Vacation.jpg"];
-var prev_val = images[0];
-var next_val = images[0];
 
-function prev() {
+async function update(change) {
+    counter += change;
     $('#slideshow_image').fadeOut(300, function () {
         /* if statement to check if the variable is at a value that is out of bounds in regards to the array. Use modulus to reset */
     });
+
+    await sleep(300);
+
+    var whichSrc = counter % 6;
+
+    $('#slideshow_image').attr('src', images[whichSrc]);
     $('#slideshow_image').fadeIn(1000);
 }
 
-function next() {
-    $('#slideshow_image').fadeOut(300, function () {
-        /* same thing as above but for positive */
-    });
-    $('#slideshow_image').fadeIn(1000);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
